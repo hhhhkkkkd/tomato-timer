39ddb99a211d448e85e9a5856fdb8b130d772365
Matsumoto Tōki <chiataowei288@icloud.com>
2026-05-19 16:30:05 +0800

    Initial Commit

---
+++ 复习计时器/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  复习计时器
+//
+//  Created by Chia Tao Wei on 19/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
